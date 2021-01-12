63baa8ffb12c8e65955beaf971add215e01553f2
oriol gomez Sentis <uri@Mac-mini-de-oriol.local>
2021-01-12 08:17:26 +0100

    add mac Safari bundle

---
+++ Hammertools/Hammertools/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Hammertools
+//
+//  Created by oriol gomez Sentis on 12/1/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
--- background.js
+++ background.js
@@ -1,38 +1,38 @@
-chrome.runtime.onInstalled.addListener(function () {
-  const topMenu = chrome.contextMenus.create({
+browser.runtime.onInstalled.addListener(function () {
+  const topMenu = browser.contextMenus.create({
     contexts: ["all"],
     title: "&Hammertools Menu",
     "id": "topMenu",
   });
-  chrome.contextMenus.create({
+  browser.contextMenus.create({
     parentId: topMenu,
     title: "Expose completely inaccessible elements",
     "id": "expose",
     contexts: ["all"],
     //onclick: exposeCompletelyInaccessibleElements,
   });
-  chrome.contextMenus.create({
+  browser.contextMenus.create({
     parentId: topMenu,
     title: "Kill all aria-&hidden",
     "id": "killariahidden",
     contexts: ["all"],
     //onclick: killAllAriaHidden,
   });
-  chrome.contextMenus.create({
+  browser.contextMenus.create({
     parentId: topMenu,
     title: "Kill all ARIA &live regions",
     "id": "killlive",
     contexts: ["all"],
     //onclick: killAllAriaLive,
   });
-  chrome.contextMenus.create({
+  browser.contextMenus.create({
     parentId: topMenu,
     title: "Kill all ARIA &applications",
     "id": "fixapplication",
     contexts: ["all"],
     //onclick: killAllAriaApplication,
   });
-  chrome.contextMenus.create({
+  browser.contextMenus.create({
     parentId: topMenu,
     title: "No idea, do all the things",
     "id": "runall",
@@ -40,7 +40,7 @@ chrome.runtime.onInstalled.addListener(function () {
     //onclick: runAll,
   });
 });
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
+browser.contextMenus.onClicked.addListener(function (info, tab) {
   let id = info.menuItemId
   if (id == "expose") exposeCompletelyInaccessibleElements(info, tab)
   if (id == "killariahidden") killAllAriaHidden(info, tab)
@@ -51,7 +51,7 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
 
 //code
 function exposeCompletelyInaccessibleElements(info, tab) {
-  chrome.tabs.executeScript(tab.id, {
+  browser.tabs.executeScript(tab.id, {
     allFrames: true,
     code: `
       for (let el of document.body.querySelectorAll(":empty:not(input):not(textarea):not([aria-label])")) {
@@ -74,7 +74,7 @@ function exposeCompletelyInaccessibleElements(info, tab) {
 }
 
 function killAllAriaHidden(info, tab) {
-  chrome.tabs.executeScript(tab.id, {
+  browser.tabs.executeScript(tab.id, {
     allFrames: true,
     code: `
       for (let el of document.querySelectorAll("[aria-hidden]")) {
@@ -85,7 +85,7 @@ function killAllAriaHidden(info, tab) {
 }
 
 function killAllAriaLive(info, tab) {
-  chrome.tabs.executeScript(tab.id, {
+  browser.tabs.executeScript(tab.id, {
     allFrames: true,
     code: `
       for (let el of document.querySelectorAll("[aria-live]")) {
@@ -96,7 +96,7 @@ function killAllAriaLive(info, tab) {
 }
 
 function killAllAriaApplication(info, tab) {
-  chrome.tabs.executeScript(tab.id, {
+  browser.tabs.executeScript(tab.id, {
     allFrames: true,
     code: `
       for (let el of document.querySelectorAll("[role=application]")) {
